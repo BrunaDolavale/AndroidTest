d7e10b3f814a5e0996d42ba3972073a845922b7b
BrunaDolavale <brunadolavalerj@gmail.com>
2018-09-13 21:40:32 -0300

    First commit

---
+++ app/src/main/java/com/example/user/assessmentbrunadolavale/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.user.assessmentbrunadolavale;
+
+import android.annotation.TargetApi;
+import android.appwidget.AppWidgetProviderInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ListView listView = (ListView)findViewById(R.id.listView);
+
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Api.BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        Api api = retrofit.create(Api.class);
+
+        Call<List<Hero>> call = api.getHeroes();
+
+        call.enqueue(new Callback<List<Hero>>() {
+            @Override
+            public void onResponse(Call<List<Hero>> call, Response<List<Hero>> response) {
+                List<Hero> heroes = response.body();
+                String[] heroNames = new String[heroes.size()];
+                for(int i = 0; i<heroes.size(); i++){
+                    heroNames[i] = heroes.get(i).getName();
+                }
+
+                listView.setAdapter(
+                        new ArrayAdapter<String>(
+                                getApplicationContext(),
+                                android.R.layout.simple_list_item_1,
+                                heroNames
+                        )
+                );
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Hero>> call, Throwable t) {
+                Toast.makeText(getApplicationContext(), t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
